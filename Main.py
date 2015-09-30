5fedf5579967d9c53bacc9679f26cfed45e137f2
Rudolf Horvath <rudolf.official@gmail.com>
2015-09-30 23:27:33 +0200

    Code cleaning and rewrite of PlanetLab scanning functions

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-__author__ = 'erudhor'
+__author__ = 'Rudolf Horvath'
 
 import sys
 
@@ -33,8 +33,8 @@ iperf_client_skeleton = "iperf -c %s -u -t %d -i %d -b %dm -f m -p %d"
 # ip(interface), port
 iperf_server_skeleton = 'iperf -s -B %s -u -p %d'
 
-used_procs = 10
-used_threads = 10
+used_procs = 100
+used_threads = 200
 
 RUN_MEASURES = ["iperf"]  # , "traceroute"]
 
@@ -51,6 +51,12 @@ Connection.connectionbuilder = \
     ConnectionBuilder(slice_name, rsa_file, None)
 
 logger = logging.getLogger()
+
+
+def main():
+    #continous_measuring()
+    scan_os_types()
+    get_scan_statistic()
 
 
 def setup_logging():
@@ -72,26 +78,6 @@ def setup_logging():
     logger.setLevel(logging.INFO)
 
 
-def main():
-    while True:
-        continous_measuring()
-
-    exit()
-
-    logger.info("Program started")
-
-    measure_iperf()
-
-    logger.info("Saving results")
-    persist()
-    logger.info("Program ened")
-    exit()
-
-    init()
-    measure()
-    persist()
-
-
 def saveOneMeasure(data):
     timeStamp = getTime().replace(":", ".")[0:-3]
     filename = 'results/%s/%s/rawTrace_%s_%s.json' % (getDate(), timeStamp[:2], getDate(), timeStamp)
@@ -104,185 +90,24 @@ def saveOneMeasure(data):
 
 
 def continous_measuring():
-    nodes = getPlanetLabNodes(slice_name)
 
-    i = 0
-
-    for node in nodes:
-        i += 1
-        i %= 5
+    def measure_node(node, i):
         log = "empty string"
         try:
             log = subprocess.check_output(["python", "SingleMeasure.py", "-n", node])
         except Exception:
             log = "Reaching node %s failed:\n%s" % (node, traceback.format_exc())
 
+        # Save log for last measure
         with open("Main.py.%d.log" % i, 'w') as f:
             f.write(log)
-        """
-        iperf_check = check_iperf(node)
-        logger.info("Iperf install check on node '%s': %s" % (node, iperf_check))
-        if "installed" not in iperf_check:
-            continue
-        akt = create_paralell_iperf(node, target1, target2)
-        akt.startMeasure()
-        akt.join()
-        data = akt.getData(False)
-        #print "Data: ", data
-        if data is not None:
-            saveOneMeasure(data)
-        """
 
-
-def create_paralell_iperf(node, target1, target2):
-    duration = 5
-    interval = 1
-    bandwidth = 20
-    port = 5200
-
-    # target ip address
-    trace_script = "traceroute -w 5.0 -q 3 %s"
-
-    # ip interface - port
-    iperf_server_script = 'iperf -s -B %s -u -p %d -i 1'
-
-    # ip address - port - duration - interval - bandwidth Mbitps
-    start_client_skeleton = "iperf -c %s -p %d -u -t %d -i %d -b %dm -f m -i 1"
-
-    paralell_measure = ParalellMeasure()
-
-    # Traceroute
-    akt = Measure(node, target1)
-    akt.setScript("traceroute", trace_script)
-    paralell_measure.addMeasure(akt, 0)
-
-    def addIperf(paralell_measure, name, target, start, duration, bandwidth, port, interval):
-        akt = Measure(target, None, "mptcp")
-        akt.setScript("iperf_server_" + name, iperf_server_script % (target, port), duration + 3)
-        paralell_measure.addMeasure(akt, start, True, duration + 2)
-
-        akt = Measure(node, target)
-        script = start_client_skeleton % (target, port, duration,
-                                          interval, bandwidth)
-        akt.setScript("iperf_client_" + name, script)
-        paralell_measure.addMeasure(akt, start + 1, )
-
-        return paralell_measure
-
-    def addIperf_oneBandwidth_scenario(akt_measure, name_prefix, target1_, target2_, bandwitdh_, start_time):
-        # Iperf
-
-        # Single 1
-        akt_measure = addIperf(akt_measure, name_prefix + "single_1", target1_,
-                               start_time, duration, bandwitdh_, port, interval)
-
-        # Single 2
-        akt_measure = addIperf(akt_measure, name_prefix + "single_2", target2_,
-                               start_time + duration + 3, duration, bandwitdh_, port, interval)
-
-        # Paralell
-        akt_measure = addIperf(akt_measure, name_prefix + "paralell_1", target1_,
-                               start_time + 2 * (duration + 3), duration, bandwitdh_, port, interval)
-        akt_measure = addIperf(akt_measure, name_prefix + "paralell_2", target2_,
-                               start_time + 2 * (duration + 3), duration, bandwitdh_, port, interval)
-
-        return akt_measure
-
-    for i in range(20, 25):
-        start_time = (i - 20) * 3 * (duration + 3)
-        paralell_measure = addIperf_oneBandwidth_scenario(paralell_measure, "bw" + str(i), target1, target2, i,
-                                                          start_time)
-
-    # for item in paralell_measure.measures:
-    #    print item["measure"].script
-    #    print item["measure"].name
-
-    return paralell_measure
-
-
-def measure_iperf():
-    global measures
-    port = 5200
-    duration = 5
-    node_names = bestNodes()[:1]
-    nodes = []
-
-    logger.info("Initializing iperf measures")
-    for node_name in node_names:
-        nodes.append(create_paralell_iperf(node_name, target1, target2))
-
-    logger.info("Starting measurements")
-    for node in nodes:
-        node.startMeasure()
-        node.join()
-
-        logger.info("-----------------------------\nMeasurement ended:")
-        logger.info(node.getData())
-
-    measures = nodes
-
-
-def init():
-    global measures, target_names, nodes
-
-    # nodes = getPlanetLabNodes(slice_name)
-    nodes = bestNodes()
-    print "number of nodes: ", len(nodes)
-    print "\tfirst node: ", nodes[0]
-
-    # Build up the needed Measures
-    for target in target_names:
-        for node in nodes:  # nodes[200:300]:
-            measures.append(TracerouteMeasure(node, target))
-
-
-def persist():
-    global results
-
-    for measure in measures:
-        data = measure.getData()
-        if data is not None:
-            results.append(data)
-
-    if len(results) == 0:
-        return
-
-    # escape : to . and remove seconds
-    timeStamp = getTime().replace(":", ".")[0:-3]
-    filename = 'results/rawTrace_%s_%s.json' % (getDate(), timeStamp)
-    with open(filename, 'w') as f:
-        f.write(json.dumps(results, indent=2))
-
-    """
-    filename = 'results/rawTrace_%s_%s.txt.gzip'%(getDate(), timeStamp)
-    with open(filename,'w') as f:
-        blob        = json.dumps(results, indent=2)
-        blob_gzip   = zlib.compress(blob, 9)
-        blob_base64 = base64.b64encode(blob_gzip)
-        f.write(blob_base64)
-    """
-
-
-def measure():
-    global measures
-
-    def connectAndMeasure(measure):
-        connected = measure.connect()
-        if connected:
-            measure.runMeasure()
-        return measure
-
-    begin = time()
-    print "runMeasurements on %d threads..." % used_threads
-    measures = thread_map(connectAndMeasure, measures, used_threads)
-    # workers = Pool(used_threads)
-    # measures2 = workers.map(connectAndMeasure, measures)
-    print "Elapsed time: %0.0f seconds" % (time() - begin)
-    suceed = reduce(
-        lambda acc, new:
-        acc + 1 if new.error == None else acc,
-        measures, 0)
-    print "Succeed measures: %d" % suceed
+    while True:
+        nodes = getPlanetLabNodes(slice_name)
+        i = 0
+        for node in nodes:
+            i = (i+1)%5
+            measure_node(node, i)
 
 
 def bestNodes():
@@ -393,25 +218,20 @@ def check_iperf(node):
     return "install failed: " + outp
 
 
-def inception(nodes):
-    return thread_map(check_iperf, nodes, used_threads)
-
-
 def testOs(node_ip):
     cmd = "cat /etc/issue"
     # uname -r --> gives some more inforamtion about kernel and architecture
     node = {"ip": node_ip}
-    online = ping(node["ip"])
-    node["online"] = online
-    if not online:
+
+    con = Connection(node["ip"])
+    con.connect()
+
+    node["online"] = con.online
+
+    if con.error is not None:
+        node["error"] = con.errorTrace.splitlines()[-1]
         return node
 
-    try:
-        con = Connection.connectionbuilder.getConnection(node["ip"])
-    except Exception:
-        error_lines = traceback.format_exc().splitlines()
-        node["error"] = error_lines[-1]
-        return node
     try:
         outp, err = con.runCommand(cmd)
     except Exception:
@@ -436,12 +256,8 @@ def scan_iperf_installations():
     nodes = getPlanetLabNodes(slice_name)
 
     print "start scanning on %d threads" % (used_threads * used_procs)
-    # results = thread_map(install_iperf, nodes, used_threads)
+    results = thread_map(install_iperf, nodes, used_threads)
     # results = proc_map(install_iperf, nodes, used_threads)
-
-    node_lists = splitList(nodes, int(len(nodes) / used_procs))
-    result_lists = proc_map(inception, node_lists, used_procs)
-    results = glueList(result_lists)
 
     print "--------------------"
     c = Counter(results)
@@ -507,31 +323,6 @@ def get_scan_statistic():
             # print errors
             # print "==============================="
             # print outp
-
-
-def splitList(list, splitLen):
-    splitted_list = []
-    i = 0
-    j = 0
-    split = []
-    for node in list:
-        split.append(node)
-        if (j) % (splitLen) == splitLen - 1:
-            splitted_list.append(split)
-            split = []
-            i += 1
-        j += 1
-
-    splitted_list.append(split)
-
-    return splitted_list
-
-
-def glueList(list_of_lists):
-    results = []
-    for list in list_of_lists:
-        results.extend(list)
-    return results
 
 
 if __name__ == "__main__":
--- utils/Old_drop.py
+++ utils/Old_drop.py
@@ -1,4 +1,160 @@
-__author__ = 'erudhor'
+__author__ = 'Rudolf Horvath'
+
+
+
+# node_lists = splitList(nodes, int(len(nodes) / used_procs))
+# result_lists = proc_map(inception, node_lists, used_procs)
+# results = glueList(result_lists)
+
+def create_paralell_iperf(node, target1, target2):
+    duration = 5
+    interval = 1
+    bandwidth = 20
+    port = 5200
+
+    # target ip address
+    trace_script = "traceroute -w 5.0 -q 3 %s"
+
+    # ip interface - port
+    iperf_server_script = 'iperf -s -B %s -u -p %d -i 1'
+
+    # ip address - port - duration - interval - bandwidth Mbitps
+    start_client_skeleton = "iperf -c %s -p %d -u -t %d -i %d -b %dm -f m -i 1"
+
+    paralell_measure = ParalellMeasure()
+
+    # Traceroute
+    akt = Measure(node, target1)
+    akt.setScript("traceroute", trace_script)
+    paralell_measure.addMeasure(akt, 0)
+
+    def addIperf(paralell_measure, name, target, start, duration, bandwidth, port, interval):
+        akt = Measure(target, None, "mptcp")
+        akt.setScript("iperf_server_" + name, iperf_server_script % (target, port), duration + 3)
+        paralell_measure.addMeasure(akt, start, True, duration + 2)
+
+        akt = Measure(node, target)
+        script = start_client_skeleton % (target, port, duration,
+                                          interval, bandwidth)
+        akt.setScript("iperf_client_" + name, script)
+        paralell_measure.addMeasure(akt, start + 1, )
+
+        return paralell_measure
+
+    def addIperf_oneBandwidth_scenario(akt_measure, name_prefix, target1_, target2_, bandwitdh_, start_time):
+        # Iperf
+
+        # Single 1
+        akt_measure = addIperf(akt_measure, name_prefix + "single_1", target1_,
+                               start_time, duration, bandwitdh_, port, interval)
+
+        # Single 2
+        akt_measure = addIperf(akt_measure, name_prefix + "single_2", target2_,
+                               start_time + duration + 3, duration, bandwitdh_, port, interval)
+
+        # Paralell
+        akt_measure = addIperf(akt_measure, name_prefix + "paralell_1", target1_,
+                               start_time + 2 * (duration + 3), duration, bandwitdh_, port, interval)
+        akt_measure = addIperf(akt_measure, name_prefix + "paralell_2", target2_,
+                               start_time + 2 * (duration + 3), duration, bandwitdh_, port, interval)
+
+        return akt_measure
+
+    for i in range(20, 25):
+        start_time = (i - 20) * 3 * (duration + 3)
+        paralell_measure = addIperf_oneBandwidth_scenario(paralell_measure, "bw" + str(i), target1, target2, i,
+                                                          start_time)
+
+    # for item in paralell_measure.measures:
+    #    print item["measure"].script
+    #    print item["measure"].name
+
+    return paralell_measure
+
+
+def measure_iperf():
+    global measures
+    port = 5200
+    duration = 5
+    node_names = bestNodes()[:1]
+    nodes = []
+
+    logger.info("Initializing iperf measures")
+    for node_name in node_names:
+        nodes.append(create_paralell_iperf(node_name, target1, target2))
+
+    logger.info("Starting measurements")
+    for node in nodes:
+        node.startMeasure()
+        node.join()
+
+        logger.info("-----------------------------\nMeasurement ended:")
+        logger.info(node.getData())
+
+    measures = nodes
+
+
+def init():
+    global measures, target_names, nodes
+
+    # nodes = getPlanetLabNodes(slice_name)
+    nodes = bestNodes()
+    print "number of nodes: ", len(nodes)
+    print "\tfirst node: ", nodes[0]
+
+    # Build up the needed Measures
+    for target in target_names:
+        for node in nodes:  # nodes[200:300]:
+            measures.append(TracerouteMeasure(node, target))
+
+
+def persist():
+    global results
+
+    for measure in measures:
+        data = measure.getData()
+        if data is not None:
+            results.append(data)
+
+    if len(results) == 0:
+        return
+
+    # escape : to . and remove seconds
+    timeStamp = getTime().replace(":", ".")[0:-3]
+    filename = 'results/rawTrace_%s_%s.json' % (getDate(), timeStamp)
+    with open(filename, 'w') as f:
+        f.write(json.dumps(results, indent=2))
+
+    """
+    filename = 'results/rawTrace_%s_%s.txt.gzip'%(getDate(), timeStamp)
+    with open(filename,'w') as f:
+        blob        = json.dumps(results, indent=2)
+        blob_gzip   = zlib.compress(blob, 9)
+        blob_base64 = base64.b64encode(blob_gzip)
+        f.write(blob_base64)
+    """
+
+
+def measure():
+    global measures
+
+    def connectAndMeasure(measure):
+        connected = measure.connect()
+        if connected:
+            measure.runMeasure()
+        return measure
+
+    begin = time()
+    print "runMeasurements on %d threads..." % used_threads
+    measures = thread_map(connectAndMeasure, measures, used_threads)
+    # workers = Pool(used_threads)
+    # measures2 = workers.map(connectAndMeasure, measures)
+    print "Elapsed time: %0.0f seconds" % (time() - begin)
+    suceed = reduce(
+        lambda acc, new:
+        acc + 1 if new.error == None else acc,
+        measures, 0)
+    print "Succeed measures: %d" % suceed
 
 
 def measure_old():
