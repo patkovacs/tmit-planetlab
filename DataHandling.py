a7c032036fc9544ddefb0ba29f490bf1b7321de4
Rudolf Horvath <rudolf.official@gmail.com>
2015-10-04 14:51:33 +0200

    Begin data handling

--- DataHandling.py
+++ DataHandling.py
@@ -1,13 +1,27 @@
 __author__ = 'Rudolf Horvath'
 __date__ = '2015.06.15'
 
-__encoding__ = 'utf-8'
-# encoding: utf-8
-
-
 import sqlite3 as sql
+import os
+import time
+import datetime
+import logging
 
-class DataBase:
+
+class SQLiteDB:
+    """
+    Fuggvenyek:
+     - TracerouteMeasure-bol allo lista kezelese:
+      - Json file keszitese
+      - Beemelese SQLite adatbazisba
+      - ellistak beemelese SQLite adatbazisba
+      - IP cimek beolvasasa
+      - DNS nevek beolvasasa
+     - Ezen funkciok akar adatbazisbol beolvasva (nyers traceroute-bol)
+     - ip cim - domain nev parok tarolasa
+     - ip cim id parositas
+     - domain nev - id parositas
+    """
 
     def __init__(self, file_name):
         self.con = sql.connect(file_name)
@@ -61,16 +75,77 @@ class DataBase:
             id = self.get_IP_ID_db(putIP)
         return id
 
-"""
-Függvények:
- - TracerouteMeasure-ből álló lista kezelése:
-  - Json file készítése
-  - Beemelése SQLite adatbázisba
-  - Éllisták beemelése SQLite adatbázisba
-  - IP címek beolvasása
-  - DNS nevek beolvasása
- - Ezen funkciók akár adatbázisból beolvasva (nyers traceroute-ból)
- - ip cím - domain név párok tárolása
- - ip cím id párosítás
- - domain név - id párosítás
-"""
+
+class MongoDB:
+
+    def __init__(self):
+        pass
+
+def _get_time_dir_touple(timestamp):
+    date = datetime.date.fromtimestamp(timestamp)
+    hour = int((timestamp/3600)%24)
+    minute = int((timestamp/60)%60)
+
+    date_str = "{:d}.{:0>2d}.{:0>2d}".format(date.year,date.month,
+                                             date.day)
+    hour_str = "{:0>2d}".format(hour)
+    file_mask = "{:d}.{:0>2d}.{:0>2d}_{:0>2d}.{:0>2d}"
+    file_str = file_mask.format(date.year,date.month,date.day,
+                                hour, minute)
+    return date_str, hour_str, file_str
+
+def read_results(results_dir="results", from_date=None, until_date=None):
+    log = logging.getLogger("read_results").info
+
+    if not os.path.exists(results_dir) or not os.path.isdir(results_dir):
+        log("path does not exists")
+        return
+
+    if until_date is None:
+        until_date = datetime.datetime.now().date()
+    if from_date is None:
+        from_date = datetime.date(1970, 1, 1)
+
+    from_str = "{:d}.{:0>2d}.{:0>2d}".format(from_date.year,
+                                             from_date.month,
+                                             from_date.day)
+    until_str = "{:d}.{:0>2d}.{:0>2d}".format(until_date.year,
+                                              until_date.month,
+                                              until_date.day)
+
+    days = os.listdir(results_dir)
+    log("until date: %s", until_date)
+    log("from date: %s", from_date)
+
+    files = []
+    for day in days:
+        if day < from_str or day > until_str:
+            log("Not in timespan: %s", day)
+            continue
+        hours = os.listdir(results_dir+"/"+day)
+        for hour in hours:
+            files.extend(os.listdir("%s/%s/%s"%(results_dir, day, hour)))
+
+    print files
+
+def get_datetime(year, month, day, hour, minute, second):
+    date = datetime.date(year, month, day)
+    time = datetime.time(hour, minute, second)
+    return datetime.datetime.combine(date, time)
+
+
+def get_epoch(date_time):
+    epoch = datetime.datetime.combine(datetime.date(1970, 1, 1),
+                                      datetime.time())
+    return int((date_time - epoch).total_seconds())
+
+
+def main():
+    from_date = datetime.date(2015, 9, 5)
+    until_date = datetime.date(2015, 10, 2)
+
+    read_results("results", from_date, until_date)
+
+
+if __name__ == "__main__":
+    main()
